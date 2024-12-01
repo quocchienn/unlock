5dc8d9159456cee57e8253f0da5199c5dc82dfdf
Quốc Chiến <123287600+quocchienn@users.noreply.github.com>
2024-12-01 12:04:37 +0700

    Update unlock_1080.js

--- unlock_1080.js
+++ unlock_1080.js
@@ -1,6 +1,8 @@
 # Shadowrocket: 2024-12-1 12:07:55
 
 let obj = JSON.parse($response.body);
+
+// Mở khóa VIP
 if (obj.data) {
     obj.data.vip = {
         "type": 2,
@@ -13,4 +15,10 @@ if (obj.data) {
     obj.data.vipType = 2;
     obj.data.vipStatus = 1;
 }
+
+// Mở khóa độ phân giải
+if (obj.data.video) {
+    obj.data.video.supportFormats = [1080, 720, 480, 360];
+}
+
 $done({ body: JSON.stringify(obj) });
